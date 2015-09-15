2364544e87fb3eacf5472684ab061628dbad6e27
jameel-rean <jameel.mohammed@reancloud.com>
2015-09-15 17:10:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='jameel',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='jameel grand',
-      author_email='jameel.mohammed@reancloud.com',
-      url='https://github.com/jameel-rean/jameel.git',
-      py_modules = ['lib'],
-     )
+      version='0.1',
+      description='python module',
+      url='http://github.com/jameel-rean/jameel',
+      author='Jameel Grand',
+      author_email='jameel@example.com',
+      license='MIT',
+      packages=['jameel'],
+      zip_safe=False)
