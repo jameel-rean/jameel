fec5249ce4e655e1e97f29c4f8ec740dcdb0cb61
Mohammad, Jameel <Jameel.Mohammad@ellucian.com>
2015-09-15 17:19:04 +0530

    ok

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from setuptools import setup
 
 setup(name='jameel',
-      version='0.1',
       description='python module',
       url='http://github.com/jameel-rean/jameel',
       author='Jameel Grand',
